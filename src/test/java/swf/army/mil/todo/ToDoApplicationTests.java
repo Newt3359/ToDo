6430866570474ace8cb83aa9f5de624138e04c77
Dennis Gingrich <dennis.j.gingrich.mil@swf.army.mil>
2025-09-26 09:45:28 -0500

    intial

---
+++ src/test/java/swf/army/mil/todo/ToDoApplicationTests.java
@@ -0,0 +1,13 @@
+package swf.army.mil.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
